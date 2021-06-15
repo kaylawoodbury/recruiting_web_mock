26d1bfb884ff381c5b40cd8b6f708b9d1b609fcb
kaylawoodbury <kaylasandoval92@gmail.com>
2021-06-15 10:55:07 +0200

    update axios set up
    Co-authored-by: jaime <jaime.cruz.hostalot@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// axios.defaults.baseURL = "http://localhost:3000/api";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+      <Footer />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
