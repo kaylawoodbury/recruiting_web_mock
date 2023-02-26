3f31a9b31276ad66daa4918707e79e53c21e19f8
Kayla Woodbury <59558092+kaylawoodbury@users.noreply.github.com>
2023-02-26 17:07:48 +0100

    fix: update the candidate details mock data to include additional fields

--- src/services/_mockApi_/candidateDetails.js
+++ src/services/_mockApi_/candidateDetails.js
@@ -1,4 +1,4 @@
-import { recruitingStage, language, hired } from "../../constants";
+import { recruitingStage, language, hired, roll } from "../../constants";
 import { subDays, subHours } from 'date-fns';
 
 const now = new Date();
@@ -14,11 +14,14 @@ class CandidateApi {
         street: '31 Street',
         zip: '111',
         email: 'carson.darrin@devias.io',
+        phoneNumber: '111-111-1111',
         firstName: 'Carson',
         lastName: 'Darrin',
         recruitingStage: recruitingStage.CONTACT,
         recruitHired: hired.NA,
         language: language.ENGLISH,
+        roll: roll.DEVELOPER,
+        other: 'linkedIn url for example or referral',
         updatedAt: subDays(subHours(now+1, 7), 1).getTime()
       },
       {
@@ -29,11 +32,14 @@ class CandidateApi {
         street: '31 Way',
         zip: '222',
         email: 'fran.perez@devias.io',
+        phoneNumber: '111-111-1111',
         firstName: 'Fran',
         lastName: 'Perez',
         recruitingStage: recruitingStage.DIALOGUE,
         recruitHired: hired.NA,
         language: language.SWEDISH,
+        roll: roll.PRODUCT_MANAGER,
+        other: undefined,
         updatedAt: subDays(subHours(now+5, 7), 1).getTime()
       },
       {
@@ -44,11 +50,14 @@ class CandidateApi {
         street: '31 Avenue',
         zip: '333',
         email: 'jie.yan.song@devias.io',
+        phoneNumber: '111-111-1111',
         firstName: 'Jie',
         lastName: 'Yan Song',
         recruitingStage: recruitingStage.INTERVIEW,
         recruitHired: hired.NA,
         language: language.GERMAN,
+        roll: roll.DATA_SCIENTIST,
+        other: undefined,
         updatedAt: subDays(subHours(now, 7), 1).getTime()
       },
       {
@@ -59,11 +68,14 @@ class CandidateApi {
         street: '31 Parkway',
         zip: '333',
         email: 'jane.rotanson@devias.io',
+        phoneNumber: '111-111-1111',
         firstName: 'Jane',
         lastName: 'Rotanson',
         recruitingStage: recruitingStage.OFFER,
         recruitHired: hired.NA,
         language: language.ENGLISH,
+        roll: roll.ENGINEERING_MANAGER,
+        other: undefined,
         updatedAt: subDays(subHours(now+2, 7), 1).getTime()
       },
       {
@@ -74,11 +86,14 @@ class CandidateApi {
         street: '31 Alley',
         zip: '333',
         email: 'miron.vitold@devias.io',
+        phoneNumber: '111-111-1111',
         firstName: 'Miron',
         lastName: 'Vitold',
         recruitingStage: recruitingStage.CLOSED,
         recruitHired: hired.YES,
         language: language.SWEDISH,
+        roll: roll.DEVELOPER,
+        other: undefined,
         updatedAt: subDays(subHours(now+9, 7), 1).getTime()
       },
     ];
